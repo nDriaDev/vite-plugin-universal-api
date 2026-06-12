e3b38d5a08fe05ebbf2d3e8739fa29a3c1d84434
Andrea Cosentino <acosentino@pccube.com>
2026-06-12 11:24:21 +0200

    fix: constant + mimetype

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,6 +7,7 @@ export const Constants = {
         CREATED: 201,
         NO_CONTENT: 204,
         BAD_REQUEST: 400,
+		UNAUTHORIZED: 401,
         FORBIDDEN: 403,
         NOT_FOUND: 404,
         METHOD_NOT_ALLOWED: 405,
